b7afe26ecbf28b8bd0cbb446cc04072335d746f1
01723710909 <137289906+01723710909@users.noreply.github.com>
2023-12-11 21:03:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import About from './Component/About/About'
+import Contaact from './Component/Contact/Contaact'
+import Home from './Component/Home/Home'
+
+function App() {
+  return (
+    <>
+     <Home></Home>
+     <Contaact></Contaact>
+     <About></About>
+     
+    </>
+  )
+}
+
+export default App
